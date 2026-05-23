0c8ac08adeb20c7b2de1f4c23d06e27615dafbce
diogosait <diogosaito33@gmail.com>
2026-05-22 20:28:40 -0400

    Update Agenda.py

--- src/Agenda.py
+++ src/Agenda.py
@@ -1,7 +1,7 @@
 import json
 import pytz # Para colocar a data no fuso-horário local
 from datetime import date, timedelta, datetime
-# _*_coding: utf-8_*_
+# -*- coding: utf-8 -*-
 
 # --- Agenda melhorada ---
 agenda = [
